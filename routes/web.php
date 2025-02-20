ec4f613f04fb96f47aba55d9f89cb94608b34a0f
= <=>
2025-02-20 18:03:45 +0700

    Productpage

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomepageController::class, 'index']);
 
+Route::prefix('product')->group(function() {
+    Route::get('/', [ProductController::class, 'index'])->name('product.index');
+    Route::get('/category/food-beverage', [ProductController::class, 'foodBeverage'])->name('product.food-beverage');
+    Route::get('/category/beauty-health', [ProductController::class, 'beautyHealth'])->name('product.beauty-health');
+    Route::get('/category/home-care', [ProductController::class, 'homeCare'])->name('product.home-care');
+    Route::get('/category/baby-kid', [ProductController::class, 'babyKids'])->name('product.baby-kid');
+});
